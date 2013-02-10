e0e8daee95d3f2dd9f2eb6f14e1d0e783cf9a55d
Todd Kaiser <marvin826@comcast.net>
2013-02-09 21:52:53 -0700

    Added working files for Udacity HTML game programming class.

---
+++ Week One/javascript inheritence.js
@@ -0,0 +1,35 @@
+Weapon = Class.extend({
+    init: function() {
+        
+    }
+});
+
+MachineGun = Weapon.extend({
+    init: function() {
+        
+    }
+});
+
+ChainGun = Weapon.extend({
+    init: function(){
+
+    }
+});
+
+Entity = Class.extend({
+    init: function(){
+
+    }
+});
+
+Teleporter = Entity.extend({
+    init: function(){
+
+    }
+});
+
+EnergyCanister = Entity.extend({
+    init: function(){
+
+    }
+});
